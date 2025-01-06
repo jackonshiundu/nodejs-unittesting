2ec578f2122723077acb60fb3268ad14e5de7821
jackonshiundu <jackonshiundu2019@gmail.com>
2025-01-06 15:24:52 +0300

    test

--- test/myFirstTest.test.js
+++ test/myFirstTest.test.js
@@ -1,3 +1,34 @@
-test("one plu two", () => {
-  expect(2 + 1).toBe(3);
+describe("number operations", () => {
+  test("one plu two", () => {
+    expect(2 + 1).toBe(3);
+  });
+  test("9 plus 4", () => {
+    expect(9 + 4).not.toBe(10);
+  });
+});
+
+describe("Matcher Methods", () => {
+  test("check if a number is undefined", () => {
+    let b = undefined;
+    expect(b).toBeFalsy();
+    expect(b).not.toBeTruthy();
+    expect(b).not.toBeDefined();
+    expect(b).toBeUndefined();
+    expect(b).not.toBeNull();
+  });
+  it("should be zeor", () => {
+    let zero = 0;
+    expect(zero).toBeFalsy();
+    expect(zero).not.toBeTruthy();
+    expect(zero).not.toBeNull();
+    expect(zero).not.toBeUndefined();
+  });
+  test("number test", () => {
+    let x = 5;
+    let y = 6;
+    expect(x + y).toBeLessThan(20);
+    expect(x + y).toBeLessThanOrEqual(20);
+    expect(x + y).toBeGreaterThan(10);
+    expect(x + y).toBeGreaterThanOrEqual(10);
+  });
 });
